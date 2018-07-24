44c0c6b9e4a37351c0c97abd7de97d65d3c772fa
Rostyslav Diakiv <rostyslav.diakiv@gmail.com>
2018-07-24 14:43:44 +0300

    Added pilots list with material

--- Airport/src/modules/pilots/components/pilots-list/pilots-list.component.ts
+++ Airport/src/modules/pilots/components/pilots-list/pilots-list.component.ts
@@ -1,12 +1,16 @@
 import {
     Component,
     OnDestroy,
-    OnInit,
+    OnInit, ViewChild,
     ViewContainerRef
 } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {PilotsService} from '../../services/index';
-import {PilotDto} from '../../../shared/models/index';
+import {PilotsService} from '../../services';
+import {PilotDto} from '../../../shared/models';
+import {merge, of as observableOf} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {ExampleHttpDao, GithubIssue} from '../table-http-example/table-http-example.component';
+import {MatPaginator, MatSort} from '@angular/material';
 
 @Component({
     selector: 'app-pilots-list',
@@ -14,6 +18,18 @@ import {PilotDto} from '../../../shared/models/index';
     styleUrls: ['./pilots-list.component.scss']
 })
 export class PilotsListComponent implements OnInit, OnDestroy {
+    displayedColumns: string[] = ['id', 'name', 'familyName', 'experience', 'age', 'dateOfBirth'];
+    exampleDatabase: ExampleHttpDao | null;
+    data: PilotDto[] = [];
+
+    resultsLength = 0;
+    isLoadingResults = true;
+    isRateLimitReached = false;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+
+    //
     public pilots: PilotDto[] = [];
     public pageTitle = 'Pilots!!!';
     public listFilter: string;
@@ -25,6 +41,44 @@ export class PilotsListComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
+        // this.exampleDatabase = new ExampleHttpDao(this.http);
+
+        // If the user changes the sort order, reset back to the first page.
+        console.log(this.sort);
+        this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+        merge(this.sort.sortChange, this.paginator.page)
+            .pipe(
+                startWith({}),
+                switchMap(() => {
+                    debugger;
+                    this.isLoadingResults = true;
+                    const pils = this._pilotsService.getPilots();
+                    return pils;
+                    // return this.exampleDatabase!.getRepoIssues(
+                    //     this.sort.active, this.sort.direction, this.paginator.pageIndex);
+                }),
+                map(data => {
+                    debugger;
+                    // Flip flag to show that loading has finished.
+                    this.isLoadingResults = false;
+                    this.isRateLimitReached = false;
+                    this.resultsLength = data.length;
+
+                    return data;
+                }),
+                catchError(() => {
+                    debugger;
+                    this.isLoadingResults = false;
+                    // Catch if the GitHub API has reached its rate limit. Return empty data.
+                    this.isRateLimitReached = true;
+                    return observableOf([]);
+                })
+            ).subscribe(data => {
+            debugger;
+            this.data = data;
+        });
+
         // this.getProducts();
         // this.listFilter = this.route.snapshot.queryParams['filterBy'] || '';
         this.listFilter = this.route.snapshot.queryParamMap.get('filterBy');
